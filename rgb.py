85c657fca435c1cb8341572080b87a02742538d0
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-05-22 18:22:10 +0000

    init'

---
+++ rgb.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by Samuel Webster
+# Created on March 2022
+# This program calculates the circumference of a circle
+#     with inputted radius
+
+
+def main():
+    # I display the values of rgb
+    # output
+    for i in range(256):
+        for j in range(256):
+            for k in range(256):
+                print("RGB({0},{1},{2})".format(i, j, k))
+
+
+if __name__ == "__main__":
+    main()
